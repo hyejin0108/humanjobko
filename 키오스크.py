d260d3376f7e74b4023e36b2a6a5d940624c8531
Hyejin0108 <svivs13@gmail.com>
2026-02-09 17:48:41 +0900

    키오스크 수정

--- 키오스크.py
+++ 키오스크.py
@@ -1,75 +1,87 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  4 11:16:06 2026
+welcome_msg = "어서오세요, %s입니다."
+input_menu_number_msg = "> 주문할 메뉴를 선택하세요. (예시. 2): "
+input_quantity_msg = "> 수량을 입력하세요"
+exit_cart_msg = "* Enter를 누르면 장바구니로 이동합니다."
+input_menu_msg = "> 주문할 메뉴를 선택하세요. (예시. 1): "
+exceed_stock_msg = "죄송합니다. 현재 %s는 %d만큼 주문가능합니다."
+add_cart_msg = "* 장바구니에 %s %d잔이 추가되었습니다."
+complete_order_msg = "주문이 완료되었습니다."
+complete_sale_msg = "영업이 종료되었습니다."
 
-@author: User
-"""
-
-#%% 실습1
-name = input("이름을 입력하세요:")
-print("어서오세요 {name}님")
-menu_number = int(input("주문할 메뉴를 선택하세요 (예시: 2)"))
-quantity = int(input(">주문 수량을 입력하세요.: "))
-
-#%% 실습2
-window_width = 40;
-store_name = "별다방"
-welcome_msg = f"어서오세요. {store_name}입니다."
-
-print('-'*window_width)
-print(welcome_msg.center(window_width-11)) # 글자수 빼기
-print('-'*window_width)
-
-#%% 최종
-
-# 1. 인삿말 출력하기
 window_width = 40
+income = 0
 store_name = "별다방"
-welcome_msg = f"어서오세요. {store_name}입니다."
-
-print('-'*window_width)
-print(welcome_msg.center(window_width-11)) # 글자수 빼기
-print('-'*window_width)
-
-
-#3. Menu
-menu_names = ['Americano','Latte','Juice']
-price_dict = {'Americano':3500,
-              'Latte':4000,
-              'Juice':4500
+menu_names = ["Americano", "Latte", "Juice"]
+stock_dict = {"Americano":10,
+              "Latte":7,
+              "Juice":5
               }
-inventory_dict = {'Americano':10,
-                  'Latte':5,
-                  'Juice':7
-                  }
-menu_format = '%2d. %10s %5d원 ( %3d 잔 )'
-print('[Menu]'.center(window_width))
-print('-'*window_width)
-for i in range(3):
-    menu_name = menu_names[i]
-    print(menu_format%(i+1, menu_name, price_dict[menu_name], inventory_dict[menu_name]))
-print('-'*window_width)
+price_dict = {"Americano":3500,
+              "Latte":4000,
+              "Juice":4500
+              }
 
-# 3. cart 생성 및 메뉴 입력받기
-cart = dict()
+def print_div_line():
+    print("-"  * window_width)
 
-for name in menu_names:
-    cart[name] = 0
+def input_menu():
+    menu_number = input(input_menu_msg)
+    if menu_number == "":
+        return -1
+    else:
+        return int(menu_number) - 1
+def quantity_stock_vaildity(quantity, menu_name):
+    return stock_dict[menu_name] >= quantity
+    
 
-print(cart)
+while sum(stock_dict.values()) > 0:
+    
+    # 메뉴판 출력
+    print_div_line()
+    print((welcome_msg%(store_name)).center(window_width-len(store_name)-8))
+    print_div_line()
+    print("[Menu]".center(window_width))
+    print_div_line()
 
-add_cart_msg = "* 장바구니에 %s %d잔 추가되었습니다."
-exceed_cart_msg = "죄송합니다. 현재 %s는 %d잔까지 주문 가능합니다."
+    for idx, name in enumerate(menu_names):
+        print(f"{idx+1}. {name:>10} {price_dict[name]:>5}원 ({stock_dict[name]:>3}잔)")
+    print_div_line()
+    
+    cart = {name : 0 for name in menu_names}
+    
+    while True:
+        print(exit_cart_msg)
+        menu_number = input_menu()
+        menu_name = menu_names[menu_number]
+    
+        if menu_number == -1: # 주문 완료
+        
+            for name, quantity in cart.items():
+                income += price_dict[name] * quantity
+                stock_dict[name] -= quantity
+                
+            del cart
+            
+            print(complete_order_msg)
+            break
+        else:
+            while True:
+                quantity = int(input(input_quantity_msg))
+                if quantity_stock_vaildity(quantity, menu_name):
+                    cart[menu_name] += quantity
+                    print(add_cart_msg%(menu_name, quantity))
+                    break
+                else:
+                    print(exceed_stock_msg%(menu_name, stock_dict[menu_name]))
+print_div_line()
+print(complete_sale_msg)
+print_div_line()
+print("영업 매출: " + str(income))
+            
+            
+                
+        
+    
 
-menu_number = int(input("주문할 메뉴를 선택하세요 (예시: 2) "))
-menu_name = menu_names[menu_number-1]
 
-quantity = int(input(">주문 수량을 입력하세요.: "))
-
-is_vaild = inventory_dict[menu_name] >= quantity
-
-if is_vaild:
-    print(add_cart_msg%(menu_name, quantity))
-    cart[menu_name] += quantity
-else:
-    print(exceed_cart_msg%(menu_name, inventory_dict[menu_name]))
+    
