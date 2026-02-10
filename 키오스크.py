e233a3647a03c126ae2ca9695e5d59b8ac2a9b8f
Hyejin0108 <svivs13@gmail.com>
2026-02-10 15:06:58 +0900

    키오스크 함수화

--- 키오스크.py
+++ 키오스크.py
@@ -1,7 +1,8 @@
 welcome_msg = "어서오세요, %s입니다."
 input_menu_number_msg = "> 주문할 메뉴를 선택하세요. (예시. 2): "
-input_quantity_msg = "> 수량을 입력하세요"
+input_quantity_msg = "> 수량을 입력하세요. "
 exit_cart_msg = "* Enter를 누르면 장바구니로 이동합니다."
+cancel_cart_msg = "* Enter를 누르면 메뉴 추가가 취소됩니다."
 input_menu_msg = "> 주문할 메뉴를 선택하세요. (예시. 1): "
 exceed_stock_msg = "죄송합니다. 현재 %s는 %d만큼 주문가능합니다."
 add_cart_msg = "* 장바구니에 %s %d잔이 추가되었습니다."
@@ -21,36 +22,124 @@ price_dict = {"Americano":3500,
               "Juice":4500
               }
 
-def print_div_line():
+#%%
+def print_div_line(window_width):
     print("-"  * window_width)
 
+def print_welcome_msg(window_width):
+    print_div_line(window_width)
+    print((welcome_msg%(store_name)).center(window_width-len(store_name)-8))
+    print_div_line(window_width)
+
+def print_menu(window_width, prices, stocks):
+    print("-"  * window_width)
+    print("[Menu]".center(window_width))
+    print_div_line(window_width)
+    for idx, name in enumerate(menu_names):
+        print(f"{idx+1}. {name:>10} {prices[name]:>5}원 ({stocks[name]:>3}잔)")
+    print_div_line(window_width)
+
+def print_cart_transition_msg():
+    print(exit_cart_msg)
+
+def print_cancel_msg():
+    print(cancel_cart_msg)
+    
+def print_add_cart_msg(menu_name, quantity):
+    print(add_cart_msg%(menu_name, quantity))
+    
+def print_exceed_msg(menu_name, stocks):
+    print(exceed_stock_msg%(menu_name, stocks[menu_name]))
+
+def print_complete_order_msg ():
+    print(complete_order_msg)
+    
+def print_close_store_msg(window_width, income):
+    print_div_line(window_width)
+    print(complete_sale_msg)
+    print_div_line(window_width)
+    print("영업 매출: " + str(income))
+    
 def input_menu():
     menu_number = input(input_menu_msg)
     if menu_number == "":
         return -1
     else:
         return int(menu_number) - 1
-def quantity_stock_vaildity(quantity, menu_name):
-    return stock_dict[menu_name] >= quantity
     
+def input_quantity():
+    quantity = input(input_quantity_msg)
+    if quantity == "":
+        return -1
+    else:
+        return int(quantity)
+    
+def is_order(quantity, menu_name, stocks):
+    return stocks[menu_name] >= quantity
 
-while sum(stock_dict.values()) > 0:
+def is_all_zero_stock(stocks):
+    return sum(stocks.values()) > 0
+
+def add_menu(names, prices, stocks, menu_name, price, quantity):
+    names.append(menu_name)
+    prices[menu_name] = price
+    stocks[menu_name] = quantity
+def remove_menu(names, prices, stocks, menu_name):
+    names.remove(menu_name)
+    del stocks[menu_name]
+    del prices[menu_name]
+    stocks[menu_name] = quantity
+
+def run_store(window_width, names, prices, stocks):
+    income = 0
+    while is_all_zero_stock(stocks):
+        print_welcome_msg(window_width)
+        print_menu(window_width, prices, stocks)
+        
+        cart = {name : 0 for name in menu_names}
+        
+        while True:
+            print_cart_transition_msg()
+            menu_number = input_menu()
+            menu_name = names[menu_number]
+        
+            if menu_number == -1: # 주문 완료
+            
+                for name, quantity in cart.items():
+                    income += prices[name] * quantity
+                    stocks[name] -= quantity
+                    
+                del cart
+                
+                print_complete_order_msg()
+                break
+            else:
+                while True:
+                    quantity = input_quantity()
+                    if quantity == -1:
+                        break
+                    elif is_order(quantity, menu_name, stocks):
+                        cart[menu_name] += quantity
+                        print_add_cart_msg(menu_name, quantity)
+                        break
+                    else:
+                        print_exceed_msg(menu_name, stocks)
+    print_close_store_msg(window_width, income)
+        
+run_store(window_width, menu_names, price_dict, stock_dict)
+
+#%%
+
+while is_all_zero_stock(stock_dict):
     
     # 메뉴판 출력
-    print_div_line()
-    print((welcome_msg%(store_name)).center(window_width-len(store_name)-8))
-    print_div_line()
-    print("[Menu]".center(window_width))
-    print_div_line()
-
-    for idx, name in enumerate(menu_names):
-        print(f"{idx+1}. {name:>10} {price_dict[name]:>5}원 ({stock_dict[name]:>3}잔)")
-    print_div_line()
+    print_welcome_msg(window_width)
+    print_menu(window_width, price_dict, stock_dict)
     
     cart = {name : 0 for name in menu_names}
     
     while True:
-        print(exit_cart_msg)
+        print_cart_transition_msg()
         menu_number = input_menu()
         menu_name = menu_names[menu_number]
     
@@ -62,22 +151,20 @@ while sum(stock_dict.values()) > 0:
                 
             del cart
             
-            print(complete_order_msg)
+            print_complete_order_msg()
             break
         else:
             while True:
-                quantity = int(input(input_quantity_msg))
-                if quantity_stock_vaildity(quantity, menu_name):
+                quantity = input_quantity()
+                if quantity == -1:
+                    break
+                elif is_order(quantity, menu_name, stock_dict):
                     cart[menu_name] += quantity
-                    print(add_cart_msg%(menu_name, quantity))
+                    print_add_cart_msg(menu_name, quantity)
                     break
                 else:
-                    print(exceed_stock_msg%(menu_name, stock_dict[menu_name]))
-print_div_line()
-print(complete_sale_msg)
-print_div_line()
-print("영업 매출: " + str(income))
-            
+                    print_exceed_msg(menu_name, stock_dict)
+print_close_store_msg(window_width, income)      
             
                 
         
